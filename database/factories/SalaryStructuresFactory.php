610dd1dd98c63dde18b3c55eba349247adcbf6c2
JENMAR ALANO <jenmaralano11@gmail.com>
2026-07-14 02:26:57 -0700

    CreateMigrationEmployee

---
+++ database/factories/SalaryStructuresFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\salary_structures;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<salary_structures>
+ */
+class SalaryStructuresFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
